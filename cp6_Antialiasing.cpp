ba5a53e9e87756ec932e276944fb564ce0c75a2e
ROOOO <ql.wang.gd@gmail.com>
2021-12-04 22:09:34 +0800

    [Fix] Bugs of chapter 6.

--- cp6_Antialiasing.cpp
+++ cp6_Antialiasing.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
+#include <cfloat>
 #include "sphere.h"
 #include "hitable_list.h"
-#include "float.h"
 #include "camera.h"
 
 vec3 color(const ray& r, hitable* world) {
 	hit_record rec;
-	if (world->hit(r, 0.0, MAXFLOAT, rec)) {
+	if (world->hit(r, 0.0, FLT_MAX, rec)) {
 		return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
 	} 
 	vec3 unit_direction = unit_vector(r.direction());
 	float t = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
+
+#ifdef _WIN32
+	#define srand48(x) srand((int)(x))
+	#define drand48() ((double)rand()/(RAND_MAX + 1.0))
+#endif
 
 int main() {
 	int nx = 200;
@@ -26,13 +31,14 @@ int main() {
 	camera cam;
 	for (int j = ny - 1; j >= 0; --j) {
 		for (int i = 0; i < nx; ++i) {
-			color col(0, 0, 0);
+			vec3 col(0, 0, 0);
 			for (int s = 0; s < ns; ++s) {
 				float u = float(i + drand48()) / float(nx);
 				float v = float(j + drand48()) / float(ny);
 				ray r = cam.get_ray(u, v);
 				col += color(r, world);
 			}
+			col /= float(ns);
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
 			int ib = int(255.99 * col[2]);
