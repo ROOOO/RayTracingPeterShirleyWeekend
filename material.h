a71128b2e561fb8d7ea0b7aaa8c75f59d134b853
ROOOO <ql.wang.gd@gmail.com>
2021-12-25 17:12:31 +0800

    [Feature] Dielectric material.

--- material.h
+++ material.h
@@ -56,3 +56,62 @@ public:
     vec3 albedo;
     float fuzz;
 };
+
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted) {
+    vec3 uv = unit_vector(v);
+    float dt = dot(uv, n);
+    // vec3 r_out_perp = ni_over_nt * (uv - n * dt);
+    // vec3 r_out_parallel = -sqrt(1.0f - dot(r_out_perp, r_out_perp)) * n;
+    // float discriminate = 1.0f - ni_over_nt * ni_over_nt * (1.0f + dt * dt - 2 * dt * dt);
+    float discriminate = 1.0f - ni_over_nt * ni_over_nt * (1.0f - dt * dt);
+    if (discriminate > 0.0f) {
+        refracted = ni_over_nt * (uv - n * dt) - sqrt(discriminate) * n;
+        return true;
+    }
+    return false;
+}
+
+float schlick(float cosine, float ref_idx) {
+    float r0 = (1.0f - ref_idx) / (1.0f + ref_idx);
+    r0 *= r0;
+    return r0 + (1.0f - r0) * powf(1.0f - cosine, 5.0f);
+}
+
+class dielectric : public material {
+public:
+    dielectric(float ri)
+        : ref_idx(ri) {
+    }
+    virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+        attenuation = vec3(1.0f, 1.0f, 1.0f);
+
+        vec3 outward_normal;
+        float ni_over_nt;
+        float cosine;
+        if (dot(r_in.direction(), rec.normal) > 0.0f) {
+            outward_normal = -rec.normal;
+            ni_over_nt = ref_idx;
+            cosine = dot(r_in.direction(), rec.normal) / r_in.direction().length();
+        } else {
+            outward_normal = rec.normal;
+            ni_over_nt = 1.0f / ref_idx;
+            cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+        }
+
+        vec3 refracted;
+        float reflect_prob;
+        if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
+            reflect_prob = schlick(cosine, ref_idx);
+        } else {
+            reflect_prob = 1.0f;
+        }
+        if (drand48() < reflect_prob) {
+            vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+            scattered = ray(rec.p, reflected);
+        } else {
+            scattered = ray(rec.p, refracted);
+        }
+        return true;
+    }
+    float ref_idx;
+};
