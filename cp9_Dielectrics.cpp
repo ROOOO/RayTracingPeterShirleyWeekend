ccf52d3f572f1e8ad8874949c53882f354d3bf5c
ROOOO <ql.wang.gd@gmail.com>
2021-12-25 17:13:51 +0800

    [Feature] Chapter 9. Dielectrics.

---
+++ cp9_Dielectrics.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cfloat>
+#include "sphere.h"
+#include "hitable_list.h"
+#include "camera.h"
+#include "material.h"
+
+vec3 color(const ray& r, hitable* world, int depth = 0) {
+	hit_record rec;
+	if (world->hit(r, 0.0001, FLT_MAX, rec)) {
+		ray scattered;
+		vec3 attenuation;
+		if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+			return attenuation * color(scattered, world, depth + 1);
+		}
+		return vec3(0, 0, 0);
+	} 
+	vec3 unit_direction = unit_vector(r.direction());
+	float t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
+
+int main() {
+	int nx = 200;
+	int ny = 100;
+	int ns = 100;
+	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+	hitable* list[5];
+	list[0] = new sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f, new lambertion(vec3(0.8f, 0.3f, 0.3f)));
+	list[1] = new sphere(vec3(0.0f, -100.5f, -1.0f), 100.0f, new lambertion(vec3(0.8f, 0.8f, 0.0f)));
+	list[2] = new sphere(vec3(1.0f, 0.0f, -1.0f), 0.5f, new metal(vec3(0.8f, 0.6f, 0.2f), 1.0f));
+	list[3] = new sphere(vec3(-1.0f, 0.0f, -1.0f), 0.5f, new dielectric(1.5f));
+	list[4] = new sphere(vec3(-1.0f, 0.0f, -1.0f), -0.45f, new dielectric(1.5f));
+	hitable* world = new hitable_list(list, 5);
+	camera cam;
+	for (int j = ny - 1; j >= 0; --j) {
+		for (int i = 0; i < nx; ++i) {
+			vec3 col(0, 0, 0);
+			for (int s = 0; s < ns; ++s) {
+				float u = float(i + drand48()) / float(nx);
+				float v = float(j + drand48()) / float(ny);
+				ray r = cam.get_ray(u, v);
+				col += color(r, world);
+			}
+			col /= float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+			int ir = int(255.99 * col[0]);
+			int ig = int(255.99 * col[1]);
+			int ib = int(255.99 * col[2]);
+			std::cout << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+}
