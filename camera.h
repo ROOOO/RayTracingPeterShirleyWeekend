72e6e89d6b67029d1e7fa3b8f0bc8c0e2bef93e3
ROOOO <ql.wang.gd@gmail.com>
2021-12-26 17:14:32 +0800

    [Feature] Camera. Positionable.

--- camera.h
+++ camera.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <math.h>
 #include "ray.h"
 
 class camera {
@@ -10,6 +11,20 @@ public:
 	    vertical = vec3(0.0, 2.0, 0.0);
 	    origin = vec3(0.0, 0.0, 0.0);
     }
+
+    camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect) {
+		float theta = vfov * M_PI / 180.0f;
+		float half_height = tan(theta / 2);
+		float half_width = aspect * half_height;
+		vec3 w = unit_vector(lookfrom - lookat);
+		vec3 u = unit_vector(cross(vup, w));
+		vec3 v = cross(w, u);
+	    origin = lookfrom;
+		lower_left_corner = origin - half_width * u - half_height * v - w;
+	    horizontal = 2.0f * half_width * u;
+	    vertical = 2.0f * half_height * v;
+    }
+
     ray get_ray(float u, float v) {
         return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
     }
